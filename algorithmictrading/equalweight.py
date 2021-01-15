3403b637a431e14b312f2e1e9e5be15f826c0f7e
Rob Mulligan <robert.f.mulligan@gmail.com>
2021-01-14 19:34:24 -0500

    added value code

--- algorithmictrading/equalweight.py
+++ algorithmictrading/equalweight.py
@@ -12,22 +12,8 @@ def make_chunks(df):
 
 def make_batch_api_call(string_chunk):
     ticker_strings = ','.join(string_chunk)
-    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=stats,quote&symbols={ticker_strings}&token={IEX_CLOUD_API_TOKEN}'
+    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=stats,quote,advanced-stats&symbols={ticker_strings}&token={IEX_CLOUD_API_TOKEN}'
     return requests.get(batch_api_call_url).json()
-
-def get_data_batch(df):
-    df_list = []
-    chunks = make_chunks(df)
-    for chunk in chunks:
-        ticker_strings = ','.join(chunk)
-        batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=stats,quote&symbols={ticker_strings}&token={IEX_CLOUD_API_TOKEN}'
-        data = requests.get(batch_api_call_url).json()
-        tickers = [k for k in data.keys()]
-        latestprices = [data[k]['quote']['latestPrice'] for k in data.keys()]
-        marketcaps = [data[k]['quote']['marketCap'] for k in data.keys()]
-        df = pd.DataFrame({'ticker': tickers, 'latest_price': latestprices, 'market_cap': marketcaps})
-        df_list.append(df)
-    return  pd.concat(df_list, ignore_index=True)
 
 def get_equal_weight_data(df):
     df_list = []
@@ -62,12 +48,65 @@ def get_momentum_data(df):
         df_list.append(df)
     return  pd.concat(df_list, ignore_index=True)
 
+def value_df_transform(df):
+    return (df.copy()
+              .assign(enterpriseValue_EBITDA= lambda x: x['enterpriseValue'] / x['EBITDA'],
+                      enterpriseValue_grossProfit= lambda x: x['enterpriseValue'] / x['grossProfit'])
+              .drop(columns=['enterpriseValue', 'EBITDA', 'grossProfit'])
+              .rename(columns={'enterpriseValue_EBITDA': 'enterpriseValue/EBITDA',
+                               'enterpriseValue_grossProfit' : 'enterpriseValue/grossProfit'})
+           )
+
+def get_value_data(df):
+    df_list = []
+    chunks = make_chunks(df)
+    for chunk in chunks:
+        data = make_batch_api_call(chunk)
+        tickers = [k for k in data.keys()]
+        latestprices = [data[k]['quote']['latestPrice'] for k in data.keys()]
+        pe_ratios = [data[k]['quote']['peRatio'] for k in data.keys()]
+        price_books = [data[k]['advanced-stats']['priceToBook'] for k in data.keys()]
+        price_sales = [data[k]['advanced-stats']['priceToSales'] for k in data.keys()]
+        enterpriseValues = [data[k]['advanced-stats']['enterpriseValue'] for k in data.keys()]
+        ebitas = [data[k]['advanced-stats']['EBITDA'] for k in data.keys()]
+        grossprofits = [data[k]['advanced-stats']['grossProfit'] for k in data.keys()]
+        df = (pd.DataFrame({'ticker': tickers, 
+                           'latest_price': latestprices, 
+                           'peRatio': pe_ratios,
+                           'priceToBook': price_books,
+                           'priceToSales': price_sales,
+                           'enterpriseValue': enterpriseValues,
+                           'EBITDA': ebitas,
+                           'grossProfit': grossprofits
+                          })
+              .pipe(value_df_transform)
+             )
+        df_list.append(df)
+    return  pd.concat(df_list, ignore_index=True)
+
 def generate_high_quality_momentum_score(df, stock_cutoff=50):
     return (df.copy()
               .set_index(['ticker', 'latest_price'])
               .rank(pct=True)
               .assign(hqm_score= lambda x: x.mean(axis='columns'))
               .sort_values('hqm_score', ascending=False)
+              .reset_index()
+              .head(stock_cutoff)
+           )
+
+def fill_missing_vals(df):
+    df = df.copy()
+    return (df.set_index('ticker')
+              .apply(lambda x: x.fillna(x.mean()))
+              .reset_index()
+           )
+
+def generate_robust_value_score(df, stock_cutoff=50):
+    return (df.copy()
+              .set_index(['ticker', 'latest_price'])
+              .rank(pct=True)
+              .assign(rv_score= lambda x: x.mean(axis='columns'))
+              .sort_values('rv_score')
               .reset_index()
               .head(stock_cutoff)
            )
@@ -99,6 +138,3 @@ if __name__ == "__main__":
 
     print(df.head())
     print(df2.head())
-
-
-    
